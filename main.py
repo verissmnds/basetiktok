b892e1e2db71c662976b1ad0cf448f10c78e3c6e
verissmnds <brunaverissimoestudos@gmail.com>
2023-08-10 15:34:45 -0300

    commit final da atividade

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 df = pd_read.csv('https://encurtador.com.br/ctUX8')
 
 print("Bruna Verissimo, graduando Comunicação Digital na Fundação Getúlio Vargas")
-print("Terceira alteração, só pra testar. Depois eu tiro, pra testar de novo hahahahaha")
-print(df)git
+
+print(df)
+
 
